3766e52174a910c9a8fa014dd66cc09217129f79
fibbbonachos <arish2k3@gmail.com>
2023-08-05 16:16:10 +0530

    update

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ const server = http.createServer((req, res) => {
     res.write("<h1>Hello from node js, welcome to my assignment page</h1>");
     res.write("</body>");
     res.write("</html>");
+  } else if (url === "/users") {
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<title>Assignment server</title>");
+    res.write("</head>");
+    res.write("<body>");
+    res.write("<h1>Users page</h1>");
+    res.write("<ol><li>josh</li><li>john</li><li>joe</li></ol>");
+    res.write("</body>");
+    res.write("</html>");
   }
 });
 
